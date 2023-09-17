295b8fb81e608bec26b95cc1e1847fc295b013d5
rosianuandrei <rosianu_andrei@hotmail.co.uk>
2023-09-17 10:23:04 +0300

    Create index.js and the public directory

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const basePath = path.join(__dirname, './public')
+
+app.use(express.static(basePath));
+
+app.listen(port, () => {
+    console.log(`App is running on port ${port}.`)
+    console.log(`Acces it here: localhost:${port}`)
+})

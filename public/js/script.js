fb49b7d8bc308b74c737633b6c7e02a452f89b4f
rosianuandrei <rosianu_andrei@hotmail.co.uk>
2023-09-23 16:49:15 +0300

    Get the elements and fetch the faceapi models through promise

---
+++ public/js/script.js
@@ -0,0 +1,19 @@
+const video = document.getElementById('videoInput');
+const button = document.getElementById('start');
+
+Promise.all([
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+]).then(start)
+
+function start() {
+
+    navigator.getUserMedia(
+        {video: {}},
+        stream => video.srcObject = stream,
+        error => console.error(error)
+    )
+
+    faceRecognizer();
+}
